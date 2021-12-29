3d31e3bbbd74f6590648e63db468c232afe78563
Agustin Gramajo <agustingramajo@gmail.com>
2021-12-29 20:20:55 -0300

    Checkout done

--- schemas/Order.ts
+++ schemas/Order.ts
@@ -6,10 +6,10 @@ export const Order = list({
   fields: {
     label: virtual({
       graphQLReturnType: 'String',
-      resolver: function(item){
+      resolver(item) {
         return `${formatMoney(item.total)}`;
-      }
-    })
+      },
+    }),
     total: integer(),
     items: relationship({ ref: 'OrderItem.order', many: true }),
     user: relationship({ ref: 'User.orders' }),
